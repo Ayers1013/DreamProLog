c4c7983c95f113d82d8b52d7ee4bd8df3d63e7f7
Szabo Kristof <krist.game13@gmail.com>
2021-03-25 17:09:42 +0100

    gnn test 1

--- debug.py
+++ debug.py
@@ -1,22 +1,40 @@
-from Env_ProLog import ProLog as Pl
 
-a=Pl()
-a.step(1)
 
-from gnn.gnn_util import *
-graph=TermGraph(PrologDecoder(a.prolog))
+def debug_1():
+    from Env_ProLog import ProLog as Pl
 
-curr_lit, path, all_goals, ext_clauses, ext_mask, _ext_perm = a.make_image()
-graph.add_clause([curr_lit], 0)
-mask = [0]
-graph.add_clause(all_goals, 1)
-mask += [0] * len(all_goals)
-for p in path:
-    graph.add_clause([p], 2)
-    mask += [0]    
-graph.ini_var = 3
-for axiom in ext_clauses:
-    graph.reset_vars()
-    graph.add_clause(axiom, 3) 
-    print(graph.node_d.items())
-mask += ext_mask
+    a=Pl()
+    a.step(1)
+
+    from gnn.gnn_util import *
+    graph=TermGraph(PrologDecoder(a.prolog))
+
+    curr_lit, path, all_goals, ext_clauses, ext_mask, _ext_perm = a.make_image()
+    graph.add_clause([curr_lit], 0)
+    mask = [0]
+    graph.add_clause(all_goals, 1)
+    mask += [0] * len(all_goals)
+    for p in path:
+        graph.add_clause([p], 2)
+        mask += [0]    
+    graph.ini_var = 3
+    for axiom in ext_clauses:
+        graph.reset_vars()
+        graph.add_clause(axiom, 3) 
+        print(graph.node_d.items())
+    mask += ext_mask
+
+def debug_2():
+    from gnn.traceback_utils import input2graph
+    from Env_ProLog import ProLog as pl
+    from gnn.network import Network, NetworkConfig
+
+    env=pl()
+    x=env.reset()
+    x=x['image']
+    x=input2graph(env.prolog, x)
+    network = Network(NetworkConfig())
+
+    print("done")
+
+
