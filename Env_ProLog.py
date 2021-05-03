1fb37112984ec27d3cf33b1d403d27006a6b4b24
Szabo Kristof <krist.game13@gmail.com>
2021-05-03 14:42:08 +0200

    adjs

--- Env_ProLog.py
+++ Env_ProLog.py
@@ -78,7 +78,7 @@ class ProLog:
         result = list(self.prolog.query(query))
         if len(result) == 0:
             self.result=-1
-            reward = -self.small_reward#-1 
+            reward = -10*self.small_reward#-1 
         else:
             self.result = result[0]["Result"]
             self.gnnInput = result[0]["GnnInput"]
