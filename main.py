db8eb06a588195d16554cb5462c8031c62719178
Szabo Kristof <krist.game13@gmail.com>
2021-07-07 11:56:12 +0200

    adjs

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def main(logdir, config):
     #tf.debugging.experimental.enable_dump_debug_info(str(logdir), tensor_debug_mode="FULL_HEALTH", circular_buffer_size=-1)
   if config.gpu_growth:
     message = 'No GPU found. To actually train on CPU remove this assert.'
-    #print(message)
+    print(message)
     #TODO
     #assert tf.config.experimental.list_physical_devices('GPU'), message
     for gpu in tf.config.experimental.list_physical_devices('GPU'):
