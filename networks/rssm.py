98bd4cc24552f97e249a26a220dcaf018befa9ab
Szabo Kristof <krist.game13@gmail.com>
2021-08-12 15:41:39 +0200

    adjs

--- networks/rssm.py
+++ networks/rssm.py
@@ -60,7 +60,7 @@ class RSSM(tools.Module):
 
   @tf.function
   def observe(self, embed, action, state=None):
-    print(action)
+    print(action.numpy())
     action=self.action_to_embed(action)
 
     print('Tracing RSSM observe function.')
