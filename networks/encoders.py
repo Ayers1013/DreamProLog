5e93595151f53ebca49f834f9aa5bc57506985f4
Szabo Kristof <krist.game13@gmail.com>
2021-08-14 17:12:59 +0200

    adjs

--- networks/encoders.py
+++ networks/encoders.py
@@ -73,7 +73,7 @@ class Encoder(tools.Module):
       
       inp=obs['action_space'][0]
       inp=tf.nest.map_structure(lambda x: tf.cast(x, dtype=tf.int32), inp)
-      action_embed(self.encoders['action_space'])(inp)
+      action_embed=self.encoders['action_space'](inp)
 
     action_embed=self.encoders['action_space'](obs['action_space'])
 
