bb3117310de9657316298e6e9819084bed193c8e
Szabo Kristof <krist.game13@gmail.com>
2021-08-23 17:54:05 +0200

    adjs

--- main.py
+++ main.py
@@ -251,5 +251,5 @@ if __name__ == '__main__':
   from controller import Controller
   ctrl=Controller(config, args.logdir)
   #ctrl.simulate()
-  ctrl.train_only_wordModel(10000)
+  ctrl.train_only_worldModel(100000)
   #ctrl.train_only(10000)
