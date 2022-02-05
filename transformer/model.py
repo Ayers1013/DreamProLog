e0a44c5be4b717ebe6f33de58f3e699ea1805b07
Ayers1013 <krist.game13@gmail.com>
2022-02-05 21:39:21 +0000

    state net testing

--- transformer/model.py
+++ transformer/model.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
-from transformer import RegressiveModel, Decoder, Encoder, RegressiveCategoricalLoss, CustomSchedule
+from .autoencoder import Model, RegressiveModel, RegressiveAutoencoder
+from .utils import *
 from misc.latent import NormalSpace
 '''
 The state autoencoder decodes a set of goals embedded into R^lxd
 Our data will come in the shape of (batch, goal_max, goal_length_max, dimension)
 '''
 
-class StateNet(tf.Module):
+class StateNet(tf.keras.Model):
     def __init__(self, 
     goal_N = 3, state_N = 3, embed_tokens = 512, goal_querry = 16, state_querry = 8, goal_length = 128, state_length = 128,
     d_model = 128, dff = 512, 
@@ -15,10 +16,10 @@ class StateNet(tf.Module):
         super().__init__()
         self.goal_querry = goal_querry
         self.goal_autoencoder = Model('regressive',
-            N = goal_N, embed_tokens = embed_tokens, querry = goal_querry, output_length = length, 
+            N = goal_N, embed_tokens = embed_tokens, querry = goal_querry, output_length = goal_length, 
             d_model = d_model, num_heads = num_heads, dff = dff, rate = dropout_rate
         )
-        self.state_autoencdoer = RegressiveAutoencoder(
+        self.state_autoencoder = RegressiveAutoencoder(
             N = state_N, querry = state_querry, output_length = state_length, latent_type = 'normal',
             d_model = d_model * d_scale, num_heads = num_heads, dff = dff * d_scale, rate = dropout_rate
         )
@@ -39,14 +40,14 @@ class StateNet(tf.Module):
             state_masks = (*state_masks, look_ahead_mask)
         
         x = tf.reshape(inp, (128*batch_size, 128))
-        goal_output, goal_masks = self.goal_autoencoder.encode(x, training)
+        goal_outputs, goal_masks = self.goal_autoencoder.encode(x, training)
 
-        goal_latent = goal_output[0]
+        goal_latent = goal_outputs[0]
         x = goal_latent.extract(training)
         x = tf.reshape(x, (batch_size, 128, self.goal_querry * 128))
         x = self.latent_dense(x)
         
-        state_latent = self.state_autoencoder(x, state_masks[0], training)
+        state_latent = self.state_autoencoder.encode(x, state_masks[0], training)
         return goal_outputs, (state_latent, x), goal_masks, state_masks
     
     def decode(self, x, mask, training):
@@ -58,26 +59,29 @@ class StateNet(tf.Module):
         
     
     def calc_loss(self, inp, target, training):
+        assert len(inp.shape)==3
+        batch_size = inp.shape[0]
         goal_outputs, state_outputs, goal_masks, state_masks = self.encode(inp, training)
         goal_sample = goal_outputs[0].extract(training)
         state_sample = state_outputs[0].extract(training)
 
-        decoded_from_goals = self.goal_autoencoder.decode((goal_sample, *goal_outputs[1:]), *goal_masks, training)
+        decoded_from_goals = self.goal_autoencoder.decode(*(goal_sample, *goal_outputs[1:]), *goal_masks, training)
         decoded_from_goals = tf.reshape(decoded_from_goals, (batch_size, 128,) + decoded_from_goals.shape[1:])
         
-        goal_latent_reconst_logits = self.state_autoencoder.decode((state_sample, *state_outputs[1:]), *state_masks, training)
+        goal_latent_reconst_logits = self.state_autoencoder.decode(*(state_sample, *state_outputs[1:]), *state_masks, training)
+        goal_latent_reconst_logits = self.out_dense(goal_latent_reconst_logits)
+        goal_latent_reconst_logits = tf.reshape(goal_latent_reconst_logits, (512, 16, 128))
         goal_latent_reconst = self.goal_latent(goal_latent_reconst_logits)
-        goal_latent_reconst_sample = goal_latent.reconst.extract(training)
-        goal_latent_reconst_sample = self.out_dense(goal_latent_reconst_sample)
-        goal_latent_reconst_sample = tf.reshape(goal_latent_reconst_sample, (batch_size*128,) + goal_latent_reconst.shape[2:])
+        goal_latent_reconst_sample = goal_latent_reconst.extract(training)
+        goal_latent_reconst_sample = tf.reshape(goal_latent_reconst_sample, (batch_size*128,) + (self.goal_querry, 128))
         
-        decoded_from_latent = self.goal_autoencoder.decode((goal_latent_reconst_sample, *goal_outputs[1:]), *goal_masks, training)
+        decoded_from_latent = self.goal_autoencoder.decode(*(goal_latent_reconst_sample, *goal_outputs[1:]), *goal_masks, training)
         decoded_from_latent = tf.reshape(decoded_from_latent, (batch_size, 128,) + decoded_from_latent.shape[1:])
         
         losses = {}
         losses['goal_autoencoder'] = self._loss(target, decoded_from_goals)
-        losses['goal_to_goal'] = self._loss(target, decoded_from_embed)#*((0.25/tf.stop_gradient(losses['goal_embedder']))**1.5)
-        losses['state_to_state'] = state_outputs[0].comparison_loss(goal_latent_reconst)
+        losses['goal_to_goal'] = self._loss(target, decoded_from_latent)#*((0.25/tf.stop_gradient(losses['goal_embedder']))**1.5)
+        losses['state_to_state'] = goal_outputs[0].compare(goal_latent_reconst)
         #self._loss2(goal_embed, goal_embed_reconst)
         return losses
     
@@ -85,7 +89,7 @@ class StateNet(tf.Module):
         inp, target = x
         losses = self.calc_loss(inp, target, training)
         scalars = {
-                'goal_embedder': 2.5,
+                'goal_autoencoder': 2.5,
                 'goal_to_goal': 0.5,
                 'state_to_state': 0.3,
             }
